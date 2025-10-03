758612a14749f96ff9c04eb71c731e734af862e0
A605\1DAW <aitorvizcayafenollar@gmail.com>
2025-10-03 17:21:31 +0200

    Ejercicos 4 UD2

---
+++ Ejercicio4UD2.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Ejercicio4UD2 {
+
+    static void main() {
+
+        int num = 433;
+
+        String num_string = Integer.toString(num);
+        String num_izquierda = num_string.substring(0,1);
+        int num_izquierda_int = Integer.parseInt(num_izquierda);
+
+        System.out.println(num_izquierda_int*100);
+    }
+}
--- practica3.java
+++ practica3.java
@@ -10,22 +10,57 @@ public class practica3 {
 
         System.out.println("***BIENVENIDO AL RATÓN JUGUETÓN***");
 
-        System.out.println("Introduce tu altura (cm)");
-        int altura = teclado.nextInt();
+        int altura = 0;
+        boolean repetir = false;
 
-        int minimo = 140;
-        int diferencia = (minimo-altura);
-        int peso_minimo = (altura*2/8);
-        int peso_maximo = 120
+        do {
+           try{
+               System.out.println("Introduce tu altura (cm)");
+               altura = teclado.nextInt();
+               repetir=false;
+           }catch(Exception err){
+               System.out.println("El formato introducido no es correcto");
+               repetir=true;
+           }
+            teclado.nextLine();
+       }while (repetir==true);
 
-        if (altura<140 && altura>0){
-            System.out.println("Lo siento, no puedes montar en la atracción. Te faltan " + diferencia + "cm de altura.");
-        }
-        if (altura>230 || altura<0){
+        int altura_minimo = 140;
+        int altura_maximo = 230;
+        int diferencia_altura = (altura_minimo-altura);
+
+        if (altura<altura_minimo && altura>0){
+            System.out.println("Lo siento, no puedes montar en la atracción. Te faltan " + diferencia_altura + "cm de altura.");
+        }else if (altura>altura_maximo || altura<0){
             System.out.println("Error de lectura. Baja de la báscula y vuelve a subir.");
         }
 
-        System.out.println("Introduce tu peso (kg)");
-        int peso = teclado.nextInt();
+        double peso = 0;
+
+        do{
+            try{
+                System.out.println("Introduce tu peso (kg)");
+                peso = teclado.nextInt();
+                repetir=false;
+            }catch (Exception err){
+                System.out.println("El formato introducido no es correcto");
+                repetir=true;
+            }
+            teclado.nextLine();
+        }while (repetir==true);
+
+        int peso_minimo = ((altura*2)/8);
+        int peso_maximo = 120;
+        double diferencia_peso_up = (peso-peso_maximo);
+        double diferencia_peso_down = (peso_minimo-peso);
+
+        if (peso>peso_maximo){
+            System.out.println("Lo siento. Sobrepasas en " + diferencia_peso_up + " kg el límite de la atracción");
+        }else if (peso_minimo>peso){
+            System.out.println("No alcanzas el límite de peso por " + diferencia_peso_down + " kg.");
+        }else{
+            System.out.println("Pesas " + peso + " kg. Peso mínimo calculado: " + peso_minimo + " kg.");
+            System.out.println("Puedes subir a la atracción.");
+        }
     }
 }
